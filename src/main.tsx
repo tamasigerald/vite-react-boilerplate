7edf025b5a2212b6aefaf23feec32c0723070697
Gerald Andrei Tamasi <tamasigerald@gmail.com>
2021-11-14 20:40:08 +0100

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,5 @@
 import { render } from 'react-dom';
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-
-import App from './App';
-import HomePage from 'pages/HomePage';
+import Routes from 'routes';
 
 const rootElement = document.getElementById('root');
-render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />}>
-                <Route path="home" element={<HomePage />}></Route>
-                <Route path="*" element={<Navigate to="/" />} />
-            </Route>
-        </Routes>
-    </BrowserRouter>,
-    rootElement,
-);
+render(<Routes />, rootElement);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,19 @@
 import { FC } from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Navigate, Routes as RouteWrapper, Route } from 'react-router-dom';
+
+import App from '../App';
 import HomePage from 'pages/HomePage';
 
 const Routes: FC = () => {
     return (
-        <>
-            <Route path="/home">
-                <HomePage />
-            </Route>
-            <Route path="*" element={<HomePage />}></Route>
-        </>
+        <BrowserRouter>
+            <RouteWrapper>
+                <Route path="/" element={<App />}>
+                    <Route path="home" element={<HomePage />}></Route>
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Route>
+            </RouteWrapper>
+        </BrowserRouter>
     );
 };
 
